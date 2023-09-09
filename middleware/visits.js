b348d8a1dca6bbf5c5d53e31591e1bc50f52f9b4
Syed Ali <alizaidi310@gmail.com>
2023-09-10 00:50:06 +0530

    prod build fix [try1]

--- middleware/visits.js
+++ middleware/visits.js
@@ -1,9 +1,15 @@
 const Url = require("../models/Url");
-const geoIp = require('geoip-lite');
+const ipInfo = require("ip-info-finder");
 
 const visitsCounter = async (code,ip) => {
   try {
     const date = new Date().toISOString();
+    let ipToAdd;
+    if(ip === '::1') {
+      ipToAdd = "NA";
+    } else {
+      ipToAdd = await ipInfo.getIPInfo('180.94.33.58');
+    }
     const url = await Url.findOneAndUpdate(
       { urlCode: code },
       {
@@ -12,7 +18,7 @@ const visitsCounter = async (code,ip) => {
             date: date,
             count: 1,
             ip: ip,
-            region: ip === '::1' ? "NA" : geoIp.lookup(ip),
+            region: ipToAdd,
           },
         },
       }
